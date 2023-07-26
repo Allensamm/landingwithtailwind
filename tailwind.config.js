007bfa634622b59b9f4a843b7705ce995c2da615
allen samuel <allensamuel569@gmail.com>
2023-07-26 02:41:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+     
+    },
+    screens: {
+      sm:'480px',
+      md:'778px',
+      lg: '977px',
+      xl:'1440px'
+    },
+  },
+  plugins: [],
+}
+
